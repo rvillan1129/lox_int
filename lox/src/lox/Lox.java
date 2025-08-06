beba2e7fb2f1ac6894f939731eaf6238b1b131e8
Rigo Villanueva <rvillanueva2@philasd.org>
2025-08-06 14:38:32 -0400

    add error handling

--- lox/src/lox/Lox.java
+++ lox/src/lox/Lox.java
@@ -51,4 +51,13 @@ public class Lox {
 			System.out.println(token);
 		}
 	}
+	
+	static void error(int line, String message) {
+		report(line, "", message);
+	}
+	
+	private static void report(int line, String where, String message) {
+		System.err.println("[line " + line + "] Error" + where + ": " + message);
+		hadError = true;
+	}
 }
